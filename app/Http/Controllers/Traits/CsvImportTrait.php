341df671e731a0a32f2d5c468ad9d7a80044724b
Sagor <mnsagor@gmail.com>
2021-01-17 01:29:20 +0600

    Complete CSV import for TD Request

--- app/Http/Controllers/Traits/CsvImportTrait.php
+++ app/Http/Controllers/Traits/CsvImportTrait.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers\Traits;
 
+use App\Http\Utills\NumberUtil;
+use App\Models\JobRequestStatus;
+use App\Models\JobType;
+use App\Models\NetworkType;
+use App\Models\OsoNumber;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use SpreadsheetReader;
@@ -11,6 +18,7 @@ trait CsvImportTrait
 {
     public function processCsvImport(Request $request)
     {
+//        dd($request->all());
         try {
             $filename = $request->input('filename', false);
             $path     = storage_path('app/csv_import/' . $filename);
@@ -44,7 +52,93 @@ trait CsvImportTrait
                 }
             }
 
+
             $for_insert = array_chunk($insert, 100);
+//            dd($for_insert);
+
+            foreach ($for_insert as $insert_item) {
+                $model::insert($insert_item);
+            }
+
+            $rows  = count($insert);
+            $table = Str::plural($modelName);
+
+            File::delete($path);
+
+            session()->flash('message', trans('global.app_imported_rows_to_table', ['rows' => $rows, 'table' => $table]));
+
+            return redirect($request->input("redirect"));
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function processCsvImportForTd(Request $request){
+//        dd($request->all());
+        $network_type_id = NetworkType::all()->where('id',config('global.171KL_NETWORK_ID'))->first();
+        $job_type_id = JobType::all()->where('id', config('global.CORE_JOB'))->first();
+        $job_request_status_id = JobRequestStatus::all()->where('id', config('global.REQUEST_STATUS_PENDING'))->first();
+
+
+
+        try {
+            $filename = $request->input('filename', false);
+            $path     = storage_path('app/csv_import/' . $filename);
+
+            $hasHeader = $request->input('hasHeader', false);
+
+            $fields = $request->input('fields', false);
+            $fields = array_flip(array_filter($fields));
+
+            $modelName = $request->input('modelName', false);
+            $model     = "App\Models\\" . $modelName;
+
+            $reader = new SpreadsheetReader($path);
+            $insert = [];
+            $pushNumberProfile = [];
+
+            foreach ($reader as $key => $row) {
+                if ($hasHeader && $key == 0) {
+                    continue;
+                }
+
+                $tmp = [];
+
+                foreach ($fields as $header => $k) {
+                    if (isset($row[$k])) {
+//                        $tmp[$header] = $row[$k];
+                        $phoneNumber = OsoNumber::all()->where('number',$row[$k])->first();
+                        $numberProfile = NumberUtil::getNumberProfile($row[$k],$network_type_id->id);
+
+                        if($numberProfile->numberOsoNumberProfiles[0]->is_queued == false
+                        && $numberProfile->numberOsoNumberProfiles[0]->is_td == config('global.INACTIVE_ID')
+                        && NumberUtil::isActive171klNumber($row[$k])){
+
+                            $tmp["requested_by_id"] = Auth::user()->id;
+                            $tmp["request_type_id"] = config('global.TEMPORARY_DISCONNECTION_REQUEST');
+                            $tmp["number"] = $row[$k];
+                            $tmp["network_type_id"] = $network_type_id->id;
+                            $tmp["job_type_id"] = $job_type_id->id;
+                            $tmp["request_status_id"] = $job_request_status_id->id;
+//                            $tmp["request_time"] = Carbon::now()->format('d-m-Y H:i:s');
+                            $tmp["request_time"] = Carbon::now();
+                            $tmp["agw_ip"] = $numberProfile->agw_ip->ip;
+                            $tmp["tid"] = $phoneNumber->tid;
+                            $tmp["call_source_code_id"] = Auth::user()->call_source_code_id;
+
+                            $numberProfile->numberOsoNumberProfiles[0]->is_queued = true;
+                            $numberProfile->push();
+                        }
+                    }
+                }
+
+                if (count($tmp) > 0) {
+                    $insert[] = $tmp;
+                }
+            }
+
+            $for_insert = array_chunk($insert, 100);
+//            dd($for_insert);
 
             foreach ($for_insert as $insert_item) {
                 $model::insert($insert_item);
@@ -92,7 +186,45 @@ trait CsvImportTrait
         $redirect = url()->previous();
 
         $routeName = 'admin.' . strtolower(Str::plural(Str::kebab($modelName))) . '.processCsvImport';
+//        dd($routeName);
+//        "admin.job-requests.processCsvImport"
 
         return view('csvImport.parseInput', compact('headers', 'filename', 'fillables', 'hasHeader', 'modelName', 'lines', 'redirect', 'routeName'));
+    }
+
+    public function parseCsvImportForTd(Request $request){
+//        dd($request);
+        $file = $request->file('csv_file');
+        $request->validate([
+            'csv_file' => 'mimes:csv,txt',
+        ]);
+
+        $path      = $file->path();
+        $hasHeader = $request->input('header', false) ? true : false;
+
+        $reader  = new SpreadsheetReader($path);
+        $headers = $reader->current();
+        $lines   = [];
+        $lines[] = $reader->next();
+        $lines[] = $reader->next();
+
+        $filename = Str::random(10) . '.csv';
+        $file->storeAs('csv_import', $filename);
+
+        $modelName     = $request->input('model', false);
+        $fullModelName = "App\Models\\" . $modelName;
+
+        $model     = new $fullModelName();
+        $fillables = $model->getFillable();
+
+        $redirect = url()->previous();
+
+        $routeName = 'admin.' . strtolower(Str::plural(Str::kebab($modelName))) . '.processTdCsvImport';
+//        dd($routeName);
+//        admin.job-requests.processTdCsvImport
+
+        return view('admin.171klNetwork.csvImport.parseTdInput', compact('headers', 'filename', 'fillables', 'hasHeader', 'modelName', 'lines', 'redirect', 'routeName'));
+
+
     }
 }
