32a0018bb819604570a405f51f080621309a24ac
Sagor <mnsagor@gmail.com>
2021-01-18 12:46:17 +0600

    Complete Document Upload for New Connection, TD, Restoration, PC request

--- resources/views/admin/171klNetwork/coreJob/newConnectionRequest.blade.php
+++ resources/views/admin/171klNetwork/coreJob/newConnectionRequest.blade.php
@@ -22,12 +22,8 @@
                     <form method="POST" action="{{ route("admin.171klnetwork.corejob.store-new-connection.request") }}" enctype="multipart/form-data">
                         @csrf
 
-{{--                        <input type="hidden" id="network_type_id" name="network_type_id" value= {{$network_types->id}} >--}}
                         <input type="hidden" id="requested_by_id" name="requested_by_id" value="{{ \Illuminate\Support\Facades\Auth::user()->id }}">
                         <input type="hidden" id="request_type_id" name="request_type_id" value="{{ config('global.NEW_CONNECTION_REQUEST') }}">
-                        {{--                        <input type="hidden" id="request_type" name="request_type" value="{{ config('global.NEW_CONNECTION_REQUEST') }}">--}}
-                        {{--                        <input type="hidden" id="network_type" name="network_type" value="{{ config('global.171KL_NETWORK') }}">--}}
-                        {{--                        <input type="hidden" id="network_sub_type" name="network_sub_type" value="{{ config('global.CORE_JOB') }}">--}}
 
 
                         <div class="row">
@@ -52,16 +48,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-4">
-                                <label>{{ "Upload Document" }}</label> <br>
-                                <input id="csv_file" type="file" class="form-control-file" name="csv_file" required disabled="ture">
 
-                                @if($errors->has('csv_file'))
-                                    <span class="help-block">
-                                <strong>{{ $errors->first('csv_file') }}</strong>
-                            </span>
-                                @endif
-                            </div>
 
                         </div>
 
@@ -103,6 +90,17 @@
                             <span class="help-block">{{ trans('cruds.jobRequest.fields.call_source_code_helper') }}</span>
                         </div>
                         @endif
+
+{{--                        Upload document file--}}
+                        <div class="form-group {{ $errors->has('file') ? 'has-error' : '' }}">
+                            <label for="file">{{ trans('cruds.jobRequest.fields.file') }}</label>
+                            <div class="needsclick dropzone" id="file-dropzone">
+                            </div>
+                            @if($errors->has('file'))
+                                <span class="help-block" role="alert">{{ $errors->first('file') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.jobRequest.fields.file_helper') }}</span>
+                        </div>
 
                         <div class="form-group">
                             <label for="note">{{ "Reference Note" }}</label>
@@ -175,13 +173,131 @@
 
         // Initialize InputMask
         $( document ).ready(function() {
+
+            function SimpleUploadAdapter(editor) {
+                editor.plugins.get('FileRepository').createUploadAdapter = function(loader) {
+                    return {
+                        upload: function() {
+                            return loader.file
+                                .then(function (file) {
+                                    return new Promise(function(resolve, reject) {
+                                        // Init request
+                                        var xhr = new XMLHttpRequest();
+                                        xhr.open('POST', '/admin/job-requests/ckmedia', true);
+                                        xhr.setRequestHeader('x-csrf-token', window._token);
+                                        xhr.setRequestHeader('Accept', 'application/json');
+                                        xhr.responseType = 'json';
+
+                                        // Init listeners
+                                        var genericErrorText = `Couldn't upload file: ${ file.name }.`;
+                                        xhr.addEventListener('error', function() { reject(genericErrorText) });
+                                        xhr.addEventListener('abort', function() { reject() });
+                                        xhr.addEventListener('load', function() {
+                                            var response = xhr.response;
+
+                                            if (!response || xhr.status !== 201) {
+                                                return reject(response && response.message ? `${genericErrorText}\n${xhr.status} ${response.message}` : `${genericErrorText}\n ${xhr.status} ${xhr.statusText}`);
+                                            }
+
+                                            $('form').append('<input type="hidden" name="ck-media[]" value="' + response.id + '">');
+
+                                            resolve({ default: response.url });
+                                        });
+
+                                        if (xhr.upload) {
+                                            xhr.upload.addEventListener('progress', function(e) {
+                                                if (e.lengthComputable) {
+                                                    loader.uploadTotal = e.total;
+                                                    loader.uploaded = e.loaded;
+                                                }
+                                            });
+                                        }
+
+                                        // Send request
+                                        var data = new FormData();
+                                        data.append('upload', file);
+                                        data.append('crud_id', '{{ $jobRequest->id ?? 0 }}');
+                                        xhr.send(data);
+                                    });
+                                })
+                        }
+                    };
+                }
+            }
+
+            var allEditors = document.querySelectorAll('.ckeditor');
+            for (var i = 0; i < allEditors.length; ++i) {
+                ClassicEditor.create(
+                    allEditors[i], {
+                        extraPlugins: [SimpleUploadAdapter]
+                    }
+                );
+            }
+
             // $("#ip").inputmask();
             $("input").change(function(){
                 document.getElementById('buttonSubmit').disabled = true;
             });
 
+
         });
 
+    </script>
+    <script>
+        var uploadedFileMap = {}
+        Dropzone.options.fileDropzone = {
+            url: '{{ route('admin.job-requests.storeMedia') }}',
+            maxFilesize: 20, // MB
+            addRemoveLinks: true,
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            params: {
+                size: 20
+            },
+            success: function (file, response) {
+                $('form').append('<input type="hidden" name="file[]" value="' + response.name + '">')
+                uploadedFileMap[file.name] = response.name
+            },
+            removedfile: function (file) {
+                file.previewElement.remove()
+                var name = ''
+                if (typeof file.file_name !== 'undefined') {
+                    name = file.file_name
+                } else {
+                    name = uploadedFileMap[file.name]
+                }
+                $('form').find('input[name="file[]"][value="' + name + '"]').remove()
+            },
+            init: function () {
+                    @if(isset($jobRequest) && $jobRequest->file)
+                var files =
+                {!! json_encode($jobRequest->file) !!}
+                    for (var i in files) {
+                    var file = files[i]
+                    this.options.addedfile.call(this, file)
+                    file.previewElement.classList.add('dz-complete')
+                    $('form').append('<input type="hidden" name="file[]" value="' + file.file_name + '">')
+                }
+                @endif
+            },
+            error: function (file, response) {
+                if ($.type(response) === 'string') {
+                    var message = response //dropzone sends it's own error messages in string
+                } else {
+                    var message = response.errors.file
+                }
+                file.previewElement.classList.add('dz-error')
+                _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+                _results = []
+                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                    node = _ref[_i]
+                    _results.push(node.textContent = message)
+                }
+
+                return _results
+            }
+        }
     </script>
 @endsection
 
